e113b011f56b307f98e04981922de87f66e25e8b
Rishav Raj <rishavraj.rr1234@gmail.com>
2026-07-19 06:20:05 +0530

    feat: add request logging middleware

--- jansetu-backend/server.js
+++ jansetu-backend/server.js
@@ -24,6 +24,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Request Logging Middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 // Routers
 app.use('/api/complaint', complaintRouter);
 app.use('/api/sensor', sensorRouter);
